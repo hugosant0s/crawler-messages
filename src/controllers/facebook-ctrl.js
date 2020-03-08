e022cefe6df22f16e47789f3d3ad0e594950633d
Hugo Santos <hugosantos_oliveira@hotmail.com>
2020-03-07 21:15:13 -0300

    Add controllers for whatsapp and facebook

---
+++ src/controllers/facebook-ctrl.js
@@ -0,0 +1,7 @@
+class FacebookController {
+    static async sendMessage(req, res) {
+        res.send({ 'message': 'Em desenvolvimento...' })
+    }
+}
+
+module.exports = FacebookController
---
+++ src/controllers/whatsapp-ctrl.js
@@ -0,0 +1,7 @@
+class WhatsappController {
+    static async sendMessage(req, res) {
+        res.send({ 'message': 'Em desenvolvimento...' })
+    }
+}
+
+module.exports = WhatsappController

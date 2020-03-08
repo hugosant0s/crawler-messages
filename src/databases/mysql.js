8245d6dcd9399b017a8c1283a923fedc216daa85
Hugo Santos <hugosantos_oliveira@hotmail.com>
2020-03-07 21:15:48 -0300

    Fix indentation of mysql.

--- src/databases/mysql.js
+++ src/databases/mysql.js
@@ -2,28 +2,26 @@ var mysql = require('mysql')
 var util = require('util')
 
 var pool = mysql.createPool({
-    connectionLimit : 10,
-    host     : process.env.MYSQL_HOST,
-    user     : process.env.MYSQL_USER,
-    password : process.env.MYSQL_PASS,
-    database : process.env.MYSQL_DATABASE
+    connectionLimit: 10,
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASS,
+    database: process.env.MYSQL_DATABASE
 })
 
 pool.getConnection((err, connection) => {
     if (err) {
         if (err.code === 'PROTOCOL_CONNECTION_LOST') {
             console.error('Database connection was closed.')
-        }
-        if (err.code === 'ER_CON_COUNT_ERROR') {
+        } else if (err.code === 'ER_CON_COUNT_ERROR') {
             console.error('Database has too many connections.')
-        }
-        if (err.code === 'ECONNREFUSED') {
+        } else if (err.code === 'ECONNREFUSED') {
             console.error('Database connection was refused.')
         }
     }
 
     if (connection) {
-        connection.release()   
+        connection.release()
     }
 
     return
